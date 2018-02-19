4ef115df9a8dbddf197872c9e6ebad6477fc1136
Simon Constans <simonc@linux.com>
2018-02-19 18:33:03 +0100

    chore: Add yarn on webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     new CopyPlugin([
       { from: 'manifest.konnector' },
       { from: 'package.json' },
+      { from: 'yarn.lock' },
       { from: 'README.md' },
       { from: 'LICENSE' }
     ])
